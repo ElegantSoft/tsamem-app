5101032847fd9dea5a5b8755cab7abf2b88f56f7
cissp <cissp@anonymouss-iMac.local>
2019-04-08 19:23:26 +0200

    last commit 8/4

---
+++ components/TryApp.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class CLassName extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Componenet Name </Text>
+            </View>
+        )
+    }
+
+}
